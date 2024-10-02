287c0133896ef4aa517c3d1f6ed976e02a77da1f
T-Tanweer04 <tanweerjamal833@gmail.com>
2024-10-02 13:32:24 +0530

    Added CreateTodo file

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function CreateTodo(){
+  return (
+    <div>
+      <input type="text" placeholder="title"/><br/>
+      <input type="text" placeholder="description"/><br/>
+      <button>Add a Todo</button>
+    </div>
+  )
+}
+
