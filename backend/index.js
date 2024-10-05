9fc53d2bbc01bb10d354f27e89a434ac4857def4
T-Tanweer04 <tanweerjamal833@gmail.com>
2024-10-05 23:27:42 +0530

    Added cors to the backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { createTodo } = require("./schema");
 const { todoModel } = require("./db");
+const cors = require("cors")
 const app = express();
 const PORT = 3000;
 app.use(express.json());
+app.use(cors());
 
 app.post("/addTodo", async function(req, res){
     const createNewTodo = req.body;
